7ceca011c53db88429ca81b4801378ec2a9eda5a
harrari <omar.harrari@gmail.com>
2017-11-06 15:51:37 +0000

    two controller

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('exemple1',[])
 
-app.controller('ctrl1',function ($scope , $http) {
+app.controller('ctrl1',function ($scope) {
     $scope.persons = ["omar","nabil","issam","MrNobody"];
     $scope.nom = "";
     $scope.newElemet ="";
@@ -8,11 +8,15 @@ app.controller('ctrl1',function ($scope , $http) {
         $scope.persons.push($scope.newElemet);
         $scope.newElemet = " ";
     }
+
+
+})
+
+app.controller("ctrl2",function ($scope,$http) {
     $scope.data = [];
     $http.get("data.json")
         .then(function (response) {
             $scope.data = response.data ;
         })
-
 })
 
